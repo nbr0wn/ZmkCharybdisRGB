7873b167f30b11dca0cf909651ba5171a054c1e6
nbrown <nbrown1@gmail.com>
2025-11-09 15:59:31 -0500

    Added docker build stuff.  Moved layouts file down to board config level

---
+++ docker/build_firmware.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script is meant to be run from inside the container.
+# Run podman_bash.sh first.  This directory will be mounted at
+# /workspaces/zmk-config/docker
+
+CONF=/tmp/zmk-config
+BUILD=/tmp/zmk-build
+OUTPUT_DIR=/workspaces/zmk-config
+
+export BOARD_ROOT=$CONF
+export DTS_ROOT=$CONF/config
+
+# Cleanup
+rm -rf $BUILD
+mkdir $BUILD
+
+# Export paths for building
+cd $CONF
+west zephyr-export
+
+# Do the build
+west build --pristine \
+	-b nice_nano_v2 \
+	-s zmk/app \
+        -d $BUILD \
+        -S "studio-rpc-usb-uart" \
+        -- \
+        -DSHIELD=charybdis_left \
+        -DZMK_CONFIG=$CONF/config \
+        -DZMK_STUDIO=y
+
+cp $BUILD/zephyr/zmk.uf2 $OUTPUT_DIR/charybdis_left.uf2
+
+rm -rf $BUILD
+mkdir $BUILD
+
+west build --pristine \
+	-b nice_nano_v2 \
+	-s zmk/app \
+        -d $BUILD \
+        -S "studio-rpc-usb-uart" \
+        -- \
+        -DSHIELD=charybdis_right \
+        -DZMK_CONFIG=$CONF/config \
+        -DZMK_STUDIO=y
+
+cp $BUILD/zephyr/zmk.uf2 $OUTPUT_DIR/charybdis_right.uf2
